a356a8933a5a65f70e08c9ccb935c05a3e1cb99f
YaAlBagdattig379 <rabiulmiah54@gmail.com>
2023-02-20 22:01:24 +0530

    added m64v6 about to get data from server which posted by client

--- src/App.js
+++ src/App.js
@@ -9,23 +9,23 @@ function App() {
     .then(data => setUsers(data))
   },[])
   const handleAddUser = (event)=>{
-    event.preventDefault()
-    const name = event.target.name.value;
-    const email = event.target.email.value;
-    console.log(name,email)
-    const user = {name,email};
-    // post data to server 
-    fetch('http://localhost:5000/user',{
+      event.preventDefault()
+      const name = event.target.name.value;
+      const email = event.target.email.value;
+      console.log(name,email)
+      const user = {name,email};
+      // post data to server 
+      fetch('http://localhost:5000/user',{
       method:'POST',
       headers:{
-        'content-type':'application/json'
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify(user)
-    })
-    .then(res => res.json())
-    .then(data => {
-      console.log(data)
-    })
+      })
+      .then(res => res.json())
+      .then(data => {
+         console.log(data)
+      })
   }
   return (
     <div className='container'>
@@ -34,8 +34,8 @@ function App() {
 
          <div className='text-center'>
            <form onSubmit={handleAddUser} className='mb-5'>
-             <input type="text" name=""placeholder='name' id="" required/> <br />
-             <input type="text" name=""placeholder='email' id="" required/> <br />
+             <input type="text" name="name" placeholder='name' id="" required/> <br />
+             <input type="email" name="email" placeholder='email' id="" required/> <br />
            <input type="submit" value="Add User" />
          </form>
          </div>
