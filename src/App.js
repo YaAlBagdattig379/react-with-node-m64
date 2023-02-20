c24a8ad6e9b2dcfc7315bd1a47eda8d203068220
YaAlBagdattig379 <rabiulmiah54@gmail.com>
2023-02-20 12:22:34 +0530

    added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 function App() {
   return (
     <div>
-         <h1>Hello empty projects of react with node m64</h1>
+         <h1>Hello Empty projects of react with node m64</h1>
     </div>
   );
 }
